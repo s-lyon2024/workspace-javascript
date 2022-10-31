e8997e222122fa46692925fcd714a6e590bb7d4d
s-lyon2024 <slyon@usca.edu>
2022-10-31 13:20:52 -0400

    oct 31

---
+++ js/changeLink.js
@@ -0,0 +1,13 @@
+// try jQuery 
+
+$(function(){   // document.ready
+
+    $('a').each(function(){
+        $(this).css("text-decoration", "line-through");
+        $(this).attr('href', "https://www.ecosia.org/");
+    });
+
+    $('img').each(function(){
+        $(this).attr('src', "img/" + 'src');
+    });
+});
