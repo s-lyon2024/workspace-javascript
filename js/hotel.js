c608018879c1544c723118e6c206816f5f47c85f
s-lyon2024 <slyon@usca.edu>
2022-11-07 12:36:13 -0500

    oct 7 index done

--- js/hotel.js
+++ js/hotel.js
@@ -16,17 +16,38 @@ console.log(test.display());
 // event
 $(function(){
     $('#submit').click(reservation);
+    $('#lists').click(displayR);
 });
 
+var guestgroup = new Array();
 function reservation(){
     var guest = {
         name: $('#name').val(),
-        numberofGuest: $('#num').val(),
-        room: $('#room').val()
+        numberofGuest: $('#Gnum').val(),
+        room: $('#room').val(),
+        checkin: $('#checkin').val(),
+        checkout: $('#checkout').val()
     };
+$('p:first').html("Thank you for your reservation!");
+guestgroup.push(guest);   // store information in array
+console.log(guestgroup);
 
     console.log(guest.name);
+    console.log(guest.checkin);
+    console.log(guest.checkout); 
     console.log(guest.numberofGuest);
     console.log(guest.room);
+ 
 
 };
+
+function displayR(){
+    $("form").append("<table>");
+    for(var i in guestgroup){
+        var theguest = guestgroup[i];
+        var row = "<tr><td>" +theguest.name+"</td><td>" + theguest.checkin + "</td><td>"+theguest.checkout +
+        "</td><td>"+theguest.numberofGuest + "</td><td>"+theguest.room +"</td></tr>";
+        $("table").append(row)
+    }
+    $("form").append("</table>");
+}
